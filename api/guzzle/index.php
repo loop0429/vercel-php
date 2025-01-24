78c1e84a9ef3205a75bbf4ea5c6715fbdf41e17f
2387ga0ka <model.f.n@gmail.com>
2025-01-24 23:16:15 +0900

    revert

--- api/guzzle/index.php
+++ api/guzzle/index.php
@@ -4,4 +4,4 @@ require_once __DIR__ . '/../../vendor/autoload.php';
 $client = new GuzzleHttp\Client();
 $res = $client->request('GET', 'https://jsonplaceholder.typicode.com/todos/1');
 
-echo json_encode($res->getBody());
+echo $res->getBody();
