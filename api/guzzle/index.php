45e83edc978a2b15e054ded0628fecaa3030c1d7
2387ga0ka <model.f.n@gmail.com>
2025-01-24 23:15:23 +0900

    json_encode

--- api/guzzle/index.php
+++ api/guzzle/index.php
@@ -4,4 +4,4 @@ require_once __DIR__ . '/../../vendor/autoload.php';
 $client = new GuzzleHttp\Client();
 $res = $client->request('GET', 'https://jsonplaceholder.typicode.com/todos/1');
 
-echo $res->getBody();
+echo json_encode($res->getBody());
