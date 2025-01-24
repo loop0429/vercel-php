0c77f0416efe6fd00451a9c9bb0fd273157257b0
2387ga0ka <model.f.n@gmail.com>
2025-01-24 23:06:26 +0900

    もう一つ掘ってみる

--- api/guzzle/index.php
+++ api/guzzle/index.php
@@ -1,5 +1,5 @@
 <?php
-require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/../../vendor/autoload.php';
 
 $client = new GuzzleHttp\Client();
 $res = $client->request('GET', 'https://vercel-php-six-kohl.vercel.app/api/sample_json/');
