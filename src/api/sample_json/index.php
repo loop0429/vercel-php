fbfd18db5f59e94abb2cef9dc121fc11befb9f0f
2387ga0ka <model.f.n@gmail.com>
2025-01-24 22:14:37 +0900

    サンプルデータ

---
+++ src/api/sample_json/index.php
@@ -0,0 +1,22 @@
+<?php
+echo json_encode([
+    'status' => 'success',
+    'data' => [
+        [
+            'id' => 1,
+            'name' => 'php',
+        ],
+        [
+            'id' => 2,
+            'name' => 'go',
+        ],
+        [
+            'id' => 3,
+            'name' => 'node',
+        ],
+        [
+            'id' => 3,
+            'name' => 'ruby',
+        ],
+    ],
+]);
