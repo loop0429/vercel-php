02aa9d703074aa7cef1476b94f965813ff776723
2387ga0ka <model.f.n@gmail.com>
2025-01-24 23:17:29 +0900

    jsonでリクエストを返す

--- api/guzzle/index.php
+++ api/guzzle/index.php
@@ -4,4 +4,5 @@ require_once __DIR__ . '/../../vendor/autoload.php';
 $client = new GuzzleHttp\Client();
 $res = $client->request('GET', 'https://jsonplaceholder.typicode.com/todos/1');
 
+header("Content-Type: application/json; charset=utf-8");
 echo $res->getBody();
