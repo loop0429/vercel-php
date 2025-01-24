767babaef205655300c1373945bf7e55b6479ea5
2387ga0ka <model.f.n@gmail.com>
2025-01-24 23:08:28 +0900

    var_dumpにしてみる

--- api/guzzle/index.php
+++ api/guzzle/index.php
@@ -4,4 +4,4 @@ require_once __DIR__ . '/../../vendor/autoload.php';
 $client = new GuzzleHttp\Client();
 $res = $client->request('GET', 'https://vercel-php-six-kohl.vercel.app/api/sample_json/');
 
-return $res;
+var_dump($res);
